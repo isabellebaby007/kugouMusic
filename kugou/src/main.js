605a8bea0c77f1fd72db6b062faec4e1620db980
isabellebaby007 <isabellezhou007@gmail.com>
2018-10-03 13:32:38 +0800

    基本代码完成

--- kugou/src/main.js
+++ kugou/src/main.js
@@ -1,12 +1,17 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
+import store from './store'
+import MintUI from 'mint-ui'
+import 'mint-ui/lib/style.css'
 Vue.config.productionTip = false
 
+
+Vue.use(MintUI)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  store: store,
   render: h => h(App)
 })
